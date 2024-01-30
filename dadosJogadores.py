3b45cb124c278a496cb0007516056bd65a623bc4
wellingtonmateusy <wellington.mateussenac@gmail.com>
2024-01-30 02:30:19 -0300

    Commit 29

--- dadosJogadores.py
+++ dadosJogadores.py
@@ -4,8 +4,10 @@ site = EsportsClient("lol")
 response = site.cargo_client.query(
     tables="ScoreboardGames=SG, ScoreboardPlayers=SP, Tournaments=T",
     join_on="SG.GameId=SP.GameId, SG.OverviewPage=T.OverviewPage",
-    fields="T.Name=Tournament, SG.DateTime_UTC, SP.Name,SP.Kills, SP.Deaths, SP.Assists, SP.CS, SP.Team, SP.TeamVs, SP.PlayerWin",
-    where="T.Name='CBLOL 2024 Split 1' AND SG.DateTime_UTC >= '2024-01-20' AND SP.Name='Titan'",
+    fields="T.Name=Tournament, SG.DateTime_UTC, SP.Name, SP.Kills, SP.P, SP.Deaths, SP.Assists, SP.CS",
+    where="T.Name='CBLOL 2024 Split 1' AND SG.DateTime_UTC >= '2024-01-20' AND SP.Name='SuperCleber'",
+    group_by="SG.GameId",
+    order_by="SG.DateTime_UTC",
 )
 
 print(response)
